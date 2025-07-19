cd47fffcf6c2ff6980377f82fa87b0b6f54885ff
DharmJarwani <dharmendhar.om@gmail.com>
2025-07-19 17:37:42 +0530

    Update Python topics: restructure chapters and add new HTML content for Chapter 2

--- src/data/pytopics.js
+++ src/data/pytopics.js
@@ -2,24 +2,20 @@
 const pytopics = [
   /* ---------- Chapter 1: Introduction ---------- */
   { chapter: 1, title: "Type A", file: "/pages/PYTHON/ch-1/CH-1_Type-a.html" },
-  { chapter: 1, title: "Type B", file: "/pages/PYTHON/ch-1/what-is-python.html" },
-  { chapter: 1, title: "Type C", file: "/pages/PYTHON/ch-1/what-is-python1.html" },
+
 
 
   /* ---------- Chapter 2: Basic Syntax ---------- */
-  { chapter: 2, title: "Type A", file: "/pages/PYTHON/ch-2/what-is-python1.html" },
-  { chapter: 2, title: "Type B", file: "/pages/PYTHON/ch-2/what-is-python2.html" },
-  { chapter: 2, title: "Type C", file: "/pages/PYTHON/ch-2/what-is-python3.html" },
+  { chapter: 2, title: "Type A", file: "/pages/PYTHON/ch-2/CH-2_Type-a.html" },
+
 
   /* ---------- Chapter 3: Operators ---------- */
   { chapter: 3, title: "Type A", file: "/pages/PYTHON/ch-3/what-is-python1.html"  },
-  { chapter: 3, title: "Type B", file: "/pages/PYTHON/ch-3/what-is-python2.html" },
-  { chapter: 3, title: "Type C", file: "/pages/PYTHON/ch-3/what-is-python3.html" },
+
 
   /* ---------- Chapter 4: Conditional Statements ---------- */
   { chapter: 4, title: "Type A", file: "/pages/PYTHON/ch-4/what-is-python.html" },
-  { chapter: 4, title: "Type B", file: "/pages/PYTHON/ch-4/nested-if.html" },
-  { chapter: 4, title: "Type C", file: "/pages/PYTHON/ch-4/match-case.html" },
+
 
   /* ---------- Chapter 5: Loops ---------- */
   { chapter: 5, title: "Type A", file: "/pages/PYTHON/ch-5/while.html" },
