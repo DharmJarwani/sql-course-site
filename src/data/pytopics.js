d9cf6ea0e106142f31d3e5b52e6a344a48c1aac6
DharmJarwani <dharmendhar.om@gmail.com>
2025-11-26 21:59:14 +0530

    Add hints for Chapter 3 Functions in hint.html and hint2.html

--- src/data/pytopics.js
+++ src/data/pytopics.js
@@ -38,6 +38,10 @@ const pytopics = [
   /* ---------- Practice section ---------- */
   { chapter: 0, title: "P1 Comparison Operators", file: "pages/PYTHON/Practice/PythonBasics-1-Equality-&-Comparison.html" },
   { chapter: 0, title: "P2 Comparison & Alignment Explained", file: "pages/PYTHON/Practice/PythonBasics-2.html" },
+  { chapter: 0, title: "P3 HHint", file: "pages/PYTHON/Practice/hint.html" },
+  { chapter: 0, title: "P4 HHint", file: "pages/PYTHON/Practice/hint2.html" },
+
+  
 ];
 
 export default pytopics;
