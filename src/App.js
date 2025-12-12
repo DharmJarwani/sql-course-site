1da43f81eadf9cbff9c7a1c8e7d7a23ea655d7ed
DharmJarwani <dharmendhar.om@gmail.com>
2025-12-12 09:11:46 +0530

    close web  temprory

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         {/* <Route path="/sqlTopics/*" element={<TutorLayoutSQL />} /> */}
-        <Route path="/pytopics/*" element={<TutorLayoutPython />} />
+        {/* <Route path="/pytopics/*" element={<TutorLayoutPython />} /> */}
       </Routes>
     </Router>
   );
